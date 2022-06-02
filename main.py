39ea53453ff2182f673c7fb5f6321e295be07501
mitraa <mitraa@pop-os.localdomain>
2022-06-02 16:03:14 +0430

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+cities_of_Iran = ["Shiraz", "Tehran", "Isfahan", "Hamedan", "Ahvaz", "Yazd"]
+print(cities_of_Iran[2])
+print(cities_of_Iran[-3])
+cities_of_Iran[0] = "Shahreraz"
+print(cities_of_Iran)
+cities_of_Iran.append("Khoramabad")
+print(cities_of_Iran)
+cities_of_Iran.extend(["Khoramabad", "Bushehr"])
+print(cities_of_Iran)
